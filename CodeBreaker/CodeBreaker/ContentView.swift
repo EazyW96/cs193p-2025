d3f37503c49bbf68e53b7e98b77374c9203faebd
Elliotte Wideman <ciaontop@gmail.com>
2026-08-29 16:35:26 -0400

    New Project

---
+++ CodeBreaker/CodeBreaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeBreaker
+//
+//  Created by  Elliotte Wideman on 8/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Eazy!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
